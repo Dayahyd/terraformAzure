08d63d74651302ce2abdf6b82729ee11d527d685
Dayahyd <94446887+Dayahyd@users.noreply.github.com>
2025-10-22 23:35:30 +0530

    Update vpn.tf

--- vpn.tf
+++ vpn.tf
@@ -3,7 +3,7 @@ provider "azurerm" { }
 
 # Create a resource group
 resource "azurerm_resource_group" "network" {
-  name     = "Preprod"
+  name     = "Preproduction"
   location = "South India"
 }
 
